8987b1ebc8efd663a27cd69d207bc1050e5b2eee
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2022-12-15 14:06:05 -0300

    Aula 3
    get e set.
    aplicações e observações.

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
-<?php //Quando não trabalha com outra linguagem não precisa fechar com o "? >"
-class Cadastro{
-	public $nome;
-	public $sobrenome;
-	public $email;
+<?php
+//Get e Set 
+class Login{
+	private $email;
+	private $senha;
 
-	public function Salvar() {
-		echo $this->nome. $this->sobrenome; //NÃO usar: concatenação antes de pontuação.
+	public function getEmail(){
+		return $this->email;
+	}
+	public function setEmail($e){
+		$this->email = $e;
+	}
+
+	public function getSenha(){
+		return $this->senha;
+	}
+	public function setSenha($s){
+		$this->senha = $s;
+	}
+
+	public function Logar() {
+		if($this->email == "teste@teste.com" and $this->senha == "123456"):
+			echo "Logado com sucesso";
+	else:
+		echo "Dados não cadastrados";
+	endif;
 	}
 }
-$pessoa = new Cadastro();
-$pessoa->nome = "Evelyn";
-$pessoa->sobrenome = "Santos";
-$pessoa->email = "eveffc@gmail.com";
-$pessoa->Salvar();
+$logar = new login();
+$logar->setEmail("teste@teste.com");
+$logar->setSenha("123456");
+$logar->Logar();
+echo "<br>";
+echo $logar->getEmail();
+
+//Observar os dois pontos após uma condição;
+//os comando devem ter a variável antecedendo;
+//Um dos usos é trabalahar com a "private"
